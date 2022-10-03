a7afc6befb606f8949ea159c8274345b49555e03
Rion Halili <rionhalili@gmail.com>
2022-10-03 14:54:46 +0200

    Implement models and connect to DB

---
+++ src/main/java/com/example/vendingmachine/model/Product.java
@@ -0,0 +1,77 @@
+package com.example.vendingmachine.model;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "amount_available")
+    private Double amountAvailable;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "seller_id")
+    private UUID sellerId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmountAvailable() {
+        return amountAvailable;
+    }
+
+    public void setAmountAvailable(Double amountAvailable) {
+        this.amountAvailable = amountAvailable;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public UUID getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(UUID sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public User getUserId() {
+        return user;
+    }
+
+    public void setUserId(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/vendingmachine/model/User.java
@@ -0,0 +1,65 @@
+package com.example.vendingmachine.model;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "user")
+    private List<Product> productList;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+}
