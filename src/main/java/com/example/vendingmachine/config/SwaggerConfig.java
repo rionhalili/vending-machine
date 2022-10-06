a5674ddb7a229c8eaeba959279f73c3ca08442d0
Rion Halili <rionhalili@gmail.com>
2022-10-06 22:36:29 +0200

    Provide Authorization button for Swagger

--- src/main/java/com/example/vendingmachine/config/SwaggerConfig.java
+++ src/main/java/com/example/vendingmachine/config/SwaggerConfig.java
@@ -7,11 +7,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -27,6 +30,7 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo())
                 .pathMapping("/")
                 .useDefaultResponseMessages(false)
+                .securitySchemes(new ArrayList<>(List.of(new ApiKey("Bearer %token", "Authorization", "Header"))))
                 .directModelSubstitute(LocalDate.class, String.class)
                 .genericModelSubstitutes(ResponseEntity.class);
     }
